9394f2e41822ebc94b646cc063f5298158acd2c7
Andrewmanoji <catchyandrewz@gmail.com>
2022-06-11 22:11:30 +0530

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { CheckBox, Input } from "rect-native-elements";
 
 export default function App() {
+  const [name, setName] = useState("");
   return (
     <View style={styles.container}>
+      <Input
+        placeholder="whats the capital of Tamil Nadu"
+        value={name}
+        onChange={(text) => setName(Text)}
+      />
+
       <Text>Our new app to present the speedometer of quiz</Text>
       <StatusBar style="auto" />
     </View>
@@ -14,8 +22,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
