c0cbe092bdf4ac24a7a7d45a670d9415ce7a69a4
Andrewmanoji <catchyandrewz@gmail.com>
2022-06-11 22:00:47 +0530

    first commit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to jij i9i start working on your app!</Text>
+      <Text>Our new app to present the speedometer of quiz</Text>
       <StatusBar style="auto" />
     </View>
   );
