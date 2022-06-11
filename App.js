193d8ca16eca5a43de1abab0c6b63c31dd86afee
MANOJ-S-2002 <manoj.23it@licet.ac.in>
2022-06-11 22:57:00 +0530

    updated

--- App.js
+++ App.js
@@ -1,17 +1,36 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState } from "react";
 import { ListViewComponent, StyleSheet, Text, View } from "react-native";
-import { CheckBox, Input } from "react-native-elements";
+import { Button, CheckBox, Input } from "react-native-elements";
 
 export default function App() {
   const [name, setName] = useState("");
+  const [correct,setCorrect]=useState(false);
+  const [false1,setFalse1]=useState(false);
+
+
+const givesCorrect=()=>{
+  setCorrect(true)
+  setFalse1(false)
+}
+const givesFalse1=()=>{
+  setFalse1(true)
+  setCorrect(false)
+}
+
   return (
     <View style={styles.container}>
       <Input
         placeholder="whats the capital of Tamil Nadu"
         value={name}
-        onChange={(text) => setName(Text)}
+        onChange={(text) => setName(text)}
       />
+      <CheckBox title="True"
+      checked={correct} 
+      onPress={givesCorrect}/>
+        <CheckBox title="False"
+      checked={false} 
+      onPress={givesFalse1}/>
 
       <Text>Our new app to present the speedometer of quiz</Text>
       <StatusBar style="auto" />
@@ -27,4 +46,3 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
 });
-
